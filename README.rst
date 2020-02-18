d79a0f7718bad3079f09a7848adfa91d660ccbfe
J.M. de la Rosa Trevin <delarosatrevin@gmail.com>
2020-02-18 23:14:54 +0100

    Minimal working version of Concept template with Flask server

--- README.rst
+++ README.rst
@@ -1,6 +1,19 @@
 
 emhub
-==========
+=====
 
 Web application for monitoring EM results
+
+
+Launching the development server
+--------------------------------
+
+.. code-block:: bash
+
+    export FLASK_APP=emhub
+    export FLASK_ENV=development
+
+    conda activate emhub-flask-py37
+
+    flask run
 
--- emhub/__init__.py
+++ emhub/__init__.py
@@ -1,1 +1,131 @@
+import os
+import json
+import base64
+from glob import glob
+from io import BytesIO
+from PIL import Image
 
+from flask import Flask, render_template, request, make_response, send_file
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+templates = [os.path.basename(f) for f in glob(os.path.join(here, 'templates', '*.html'))]
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'emhub.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/index')
+    def index():
+        return render_template('main.html')
+
+    @app.route('/projects')
+    def projects():
+        return render_template('projects.html')
+
+    @app.route('/micrographs')
+    def micrographs():
+        return render_template('micrographs.html')
+
+    def send_json_data(data):
+        resp = make_response(json.dumps(data))
+        resp.status_code = 200
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+
+    @app.route('/get_mic', methods=['POST'])
+    def get_mic():
+        """
+t20-tutorial/imgMicThumbs/14sep05c_c_00003gr_00014sq_00010hl_00002es.frames_thumbnail.png
+t20-tutorial/imgMicThumbs/14sep05c_c_00003gr_00014sq_00011hl_00002es.frames_thumbnail.png
+t20-tutorial/imgMicThumbs/14sep05c_c_00003gr_00014sq_00011hl_00003es.frames_thumbnail.png
+t20-tutorial/imgMicThumbs/14sep05c_c_00003gr_00014sq_00011hl_00004es.frames_thumbnail.png
+
+t20-tutorial/imgPsdThumbs/14sep05c_c_00003gr_00014sq_00010hl_00002es.frames_aligned_mic_ctfEstimation.png
+t20-tutorial/imgPsdThumbs/14sep05c_c_00003gr_00014sq_00011hl_00002es.frames_aligned_mic_ctfEstimation.png
+t20-tutorial/imgPsdThumbs/14sep05c_c_00003gr_00014sq_00011hl_00003es.frames_aligned_mic_ctfEstimation.png
+t20-tutorial/imgPsdThumbs/14sep05c_c_00003gr_00014sq_00011hl_00004es.frames_aligned_mic_ctfEstimation.png
+
+t20-tutorial/imgShiftThumbs/14sep05c_c_00003gr_00014sq_00010hl_00002es.frames_global_shifts.png
+t20-tutorial/imgShiftThumbs/14sep05c_c_00003gr_00014sq_00011hl_00002es.frames_global_shifts.png
+t20-tutorial/imgShiftThumbs/14sep05c_c_00003gr_00014sq_00011hl_00003es.frames_global_shifts.png
+t20-tutorial/imgShiftThumbs/14sep05c_c_00003gr_00014sq_00011hl_00004es.frames_global_shifts.png
+
+        :return:
+        """
+        micId = int(request.form['micId'])
+        return send_json_data(get_mic_prefix(micId))
+
+    @app.route('/get_mic_thumb', methods=['POST'])
+    def get_mic_thumb():
+        micId = int(request.form['micId'])
+        micThumb = get_micthumb_fn(micId)
+        encoded = fn_to_base64(micThumb)
+        return send_json_data({'thumbnail': micThumb, 'base64': encoded})
+
+    @app.route('/get_content', methods=['POST'])
+    def get_content():
+        content_template = request.form['content_id'] + '.html'
+
+        if content_template in templates:
+            return render_template(content_template)
+
+        return "<h1>Template '%s' not found</h1>" % content_template
+
+    return app
+
+
+def fn_to_base64(filename):
+    """ Read the image filename as a PIL image
+    and encode it as base64.
+    """
+    img = Image.open(filename)
+    encoded = pil_to_base64(img)
+    img.close()
+    return encoded
+
+
+def pil_to_base64(pil_img):
+    """ Encode as base64 the PIL image to be
+    returned as an AJAX response.
+    """
+    img_io = BytesIO()
+    pil_img.save(img_io, format='PNG')
+    return base64.b64encode(img_io.getvalue()).decode("utf-8")
+
+
+def get_mic_prefix(micId):
+    micDict = {
+        1: '14sep05c_c_00003gr_00014sq_00010hl_00002es.frames',
+        2: '14sep05c_c_00003gr_00014sq_00011hl_00002es.frames',
+        3: '14sep05c_c_00003gr_00014sq_00011hl_00003es.frames',
+        4: '14sep05c_c_00003gr_00014sq_00011hl_00004es.frames'
+    }
+    return micDict.get(micId)
+
+
+def get_micthumb_fn(micId):
+    return get_fn('imgMicThumbs/%s_thumbnail.png' % get_mic_prefix(micId))
+
+
+def get_fn(basename):
+    return "emdash/static/t20-tutorial/" + basename
