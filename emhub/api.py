90d64b3be53e5954129e5e807e8c6769fab78a5d
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-06-03 19:59:02 +0200

    New Applications layout and creating/updating Templates

--- emhub/api.py
+++ emhub/api.py
@@ -60,26 +60,22 @@ def get_users():
 # ---------------------------- APPLICATIONS -----------------------------------
 @api_bp.route('/create_template', methods=['POST'])
 def create_template():
-    pass
+    return handle_template(app.dm.create_template)
 
 @api_bp.route('/get_templates', methods=['POST'])
 def get_templates():
     return send_json_data({'error': 'Not implemented'})
 
+
 @api_bp.route('/update_template', methods=['POST'])
 def update_template():
-    try:
-        if not request.json:
-            raise Exception("Expecting JSON request.")
-        template = app.dm.update_template(**request.json['attrs'])
-        return send_json_data({'template': template.json()})
-    except Exception as e:
-        print(e)
-        return send_json_data({'error': 'Raised exception: %s' % e})
+    return handle_template(app.dm.update_template)
+
 
 @api_bp.route('/create_application', methods=['POST'])
 def create_application():
     pass
+
 
 @api_bp.route('/get_applications', methods=['POST'])
 def get_applications():
@@ -139,12 +135,19 @@ def filter_from_attrs(items):
     return send_json_data(sessions)
 
 
-def handle_booking(result_key, booking_func, booking_transform=None):
+def _handle_item(handle_func, result_key):
     try:
         if not request.json:
             raise Exception("Expecting JSON request.")
-        attrs = request.json['attrs']
+        result = handle_func(**request.json['attrs'])
+        return send_json_data({result_key: result})
+    except Exception as e:
+        print(e)
+        return send_json_data({'error': 'Raised exception: %s' % e})
 
+
+def handle_booking(result_key, booking_func, booking_transform=None):
+    def handle(**attrs):
         def _fix_date(date_key):
             if date_key in attrs:
                 attrs[date_key] = datetime_from_isoformat(attrs[date_key])
@@ -155,9 +158,14 @@ def handle_booking(result_key, booking_func, booking_transform=None):
         if attrs.get('repeat_value', 'no') != 'no':
             _fix_date('repeat_stop')
 
-        result = booking_func(**attrs)
         bt = booking_transform or app.dc.booking_to_event
-        return send_json_data({result_key: [bt(b) for b in result]})
-    except Exception as e:
-        print(e)
-        return send_json_data({'error': 'Raised exception: %s' % e})
+        return [bt(b) for b in booking_func(**attrs)]
+
+    return _handle_item(handle, result_key)
+
+
+def handle_template(template_func):
+    def handle(**attrs):
+        return template_func(**attrs).json()
+
+    return _handle_item(handle, 'template')
