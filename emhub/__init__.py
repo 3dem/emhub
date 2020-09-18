65c100d2273e601fe09889bd5bde3a505b16b517
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-09-18 12:08:58 +0200

    Allow to set title via config.py

--- emhub/__init__.py
+++ emhub/__init__.py
@@ -105,6 +105,7 @@ def create_app(test_config=None):
 
         kwargs['is_devel'] = app.is_devel
         kwargs['version'] = __version__
+        kwargs['emhub_title'] = app.config.get('EMHUB_TITLE', '')
 
         return flask.render_template('main.html', **kwargs)
 
