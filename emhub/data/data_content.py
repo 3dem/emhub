e5dd8f1fc7f5021db4edb96d400fec6940cf31b8
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2020-09-22 17:03:07 +0200

    More on importing users from portal

--- emhub/data/data_content.py
+++ emhub/data/data_content.py
@@ -300,27 +300,35 @@ class DataContent:
     def get_portal_users_list(self, **kwargs):
         dm = self.app.dm
         users = []
+        status = kwargs.get('status', None)
+        do_import = 'import' in kwargs
 
         for pu in self.app.sll_pm.fetchAccountsJson():
             user = dm.get_user_by(email=pu['email'])
 
             if user is None:
-                users.append(pu)
-
                 invoiceRef = pu['invoice_ref']
-                if not pu['pi']:
-                    pi = dm.get_user_by(email=invoiceRef)
-                    if pi is None:
-                        pu['status'] = 'Error: Missing PI'
-                    else:
-                        pu['status'] = 'Ready: user'
-                else:
-                    if invoiceRef.strip():
-                        pu['status'] = 'Ready: pi'
-                    else:
-                        pu['status'] = 'Error: Missing Invoice Reference'
 
-        return {'portal_users': users}
+                if pu['status'] == 'enabled':
+                    if not pu['pi']:
+                        pi = dm.get_user_by(email=invoiceRef)
+                        if pi is None:
+                            pu['status'] = 'error: Missing PI'
+                        else:
+                            pu['status'] = 'ready: user'
+                    else:
+                        if invoiceRef.strip():
+                            pu['status'] = 'ready: pi'
+                        else:
+                            pu['status'] = 'error: Missing Invoice Reference'
+
+                if status is None or pu['status'].startswith(status):
+                    users.append(pu)
+
+        return {'portal_users': users,
+                'status': status,
+                'do_import': do_import
+                }
 
     # --------------------- Internal  helper methods ---------------------------
     def booking_to_event(self, booking):
