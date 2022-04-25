658315d7e22aeaa561fdb57081434f7633c6fa87
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2022-04-25 15:33:12 +0200

    Stored form changes in the update script

--- emhub/client/scripts/update_pucks_and_storage_20220421.py
+++ emhub/client/scripts/update_pucks_and_storage_20220421.py
@@ -285,6 +285,144 @@ COLOR_MAP = {
     'Silver': 'Gainsboro'
 }
 
+NEW_SCREENING = """{
+    "title": "Screening",
+    "sections": [
+        {
+            "label": "Grids Comments",
+            "params": [
+                {
+                    "id": "grids_table",
+                    "label": "Grids Table",
+                    "type": "table",
+                    "columns": [
+                        {
+                            "id": "gridbox_label",
+                            "label": "Grid Label"
+                        },
+                        {
+                            "id": "sample",
+                            "label": "Sample",
+                            "type": "text"
+                        },
+                        {
+                            "id": "comments",
+                            "label": "Comments",
+                            "type": "text"
+                        }
+                    ],
+                    "min_rows": 5
+                }
+            ]
+        },
+        {
+            "label": "Report Images",
+            "params": [
+                {
+                    "id": "images_table",
+                    "label": "Images Table",
+                    "type": "table",
+                    "columns": [
+                        {
+                            "id": "image_title",
+                            "label": "Image Title"
+                        },
+                        {
+                            "id": "image_description",
+                            "label": "Image Description",
+                            "type": "text"
+                        },
+                        {
+                            "id": "image_file",
+                            "label": "Image File",
+                            "type": "file_image"
+                        }
+                    ],
+                    "min_rows": 5
+                }
+            ]
+        }
+    ]
+}"""
+
+NEW_STORAGE = """{
+    "title": "Grids Storage",
+    "params": [
+        {
+            "id": "grids_storage_table",
+            "label": "Grids Storage Table",
+            "type": "table",
+            "columns": [
+                {
+                    "id": "gridbox_label",
+                    "label": "GridBox Label"
+                },
+                {
+                    "id": "puck_id",
+                    "label": "Puck",
+                    "type": "custom",
+                    "template": "param_select_puck.html"
+                },
+                {
+                    "id": "box_position",
+                    "label": "Box Position",
+                    "enum": {
+                        "choices": [
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9,
+                            10,
+                            11,
+                            12
+                        ],
+                        "display": "combo"
+                    }
+                },
+                {
+                    "id": "grid_position",
+                    "label": "Grid Position",
+                    "enum": {
+                        "choices": [
+                            1,
+                            2,
+                            3,
+                            4
+                        ],
+                        "display": "combo",
+                        "multiple": true
+                    }
+                },
+                {
+                    "id": "sample",
+                    "label": "Sample",
+                    "type": "text"
+                },
+                {
+                    "id": "sessions",
+                    "label": "Session(s)"
+                },
+                {
+                    "id": "atlas",
+                    "label": "Atlas",
+                    "type": "bool"
+                },
+                {
+                    "id": "EPU",
+                    "label": "EPU",
+                    "type": "bool"
+                }
+            ],
+            "min_rows": 5
+        }
+    ]
+}"""
+
 def _color(c):
     return COLOR_MAP.get(c, c)
 
