03b575459f58c809fe5ec166f76761465e353476
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2022-04-25 15:24:04 +0200

    Update in client script to insert new pucks

--- emhub/client/scripts/update_pucks_and_storage_20220421.py
+++ emhub/client/scripts/update_pucks_and_storage_20220421.py
@@ -38,6 +38,7 @@ import subprocess
 import datetime as dt
 import tempfile
 from pprint import pprint
+from io import StringIO
 
 
 from emhub.client import open_client, config
@@ -152,6 +153,180 @@ def update_storage_entries(locDict):
                     dc.request('update_entry', jsonData={'attrs': entry})
 
 
+NEW_PUCKS = """
+201	D1C1P1	SciLifeLab_0201	Red	1	10-01	1	group	Black
+202	D1C1P2	SciLifeLab_0202	Orange	1	10-01	1	group
+203	D1C1P3	SciLifeLab_0203	Green	1	10-01	1	group
+204	D1C1P4	SciLifeLab_0204	Navy	1	10-01	1	group
+205	D1C1P5	SciLifeLab_0205	Lilac	1	10-01	1	group
+206	D1C1P6	SciLifeLab_0206	Black	1	10-01	1	group
+207	D1C1P7	SciLifeLab_0207	Silver	1	10-01	1	group
+208	D1C1P8	SciLifeLab_0208	Pink	1	10-01	1	group
+209	D1C1P9	SciLifeLab_0209	Blue	1	10-01	1	group
+210	D1C1P10	SciLifeLab_0210	Gold	1	10-01	1	group
+211	D1C2P1	SciLifeLab_0211	Red	1	10-02	1	group	Green
+212	D1C2P2	SciLifeLab_0212	Orange	1	10-02	1	group
+213	D1C2P3	SciLifeLab_0213	Green	1	10-02	1	group
+214	D1C2P4	SciLifeLab_0214	Navy	1	10-02	1	group
+215	D1C2P5	SciLifeLab_0215	Lilac	1	10-02	1	group
+216	D1C2P6	SciLifeLab_0216	Black	1	10-02	1	group
+217	D1C2P7	SciLifeLab_0217	Silver	1	10-02	1	group
+218	D1C2P8	SciLifeLab_0218	Pink	1	10-02	1	group
+219	D1C2P9	SciLifeLab_0219	Blue	1	10-02	1	group
+220	D1C2P10	SciLifeLab_0220	Gold	1	10-02	1	group
+221	D1C3P1	SciLifeLab_0221	Red	1	10-03	1	group	Pink
+222	D1C3P2	SciLifeLab_0222	Orange	1	10-03	1	group
+223	D1C3P3	SciLifeLab_0223	Green	1	10-03	1	group
+224	D1C3P4	SciLifeLab_0224	Navy	1	10-03	1	group
+225	D1C3P5	SciLifeLab_0225	Lilac	1	10-03	1	group
+226	D1C3P6	SciLifeLab_0226	Black	1	10-03	1	group
+227	D1C3P7	SciLifeLab_0227	Silver	1	10-03	1	group
+228	D1C3P8	SciLifeLab_0228	Pink	1	10-03	1	group
+229	D1C3P9	SciLifeLab_0229	Blue	1	10-03	1	group
+230	D1C3P10	SciLifeLab_0230	Gold	1	10-03	1	group
+231	D1C4P1	SciLifeLab_0231	Red	1	10-04	1	group	Red
+232	D1C4P2	SciLifeLab_0232	Orange	1	10-04	1	group
+233	D1C4P3	SciLifeLab_0233	Green	1	10-04	1	group
+234	D1C4P4	SciLifeLab_0234	Navy	1	10-04	1	group
+235	D1C4P5	SciLifeLab_0235	Lilac	1	10-04	1	group
+236	D1C4P6	SciLifeLab_0236	Black	1	10-04	1	group
+237	D1C4P7	SciLifeLab_0237	Silver	1	10-04	1	group
+238	D1C4P8	SciLifeLab_0238	Pink	1	10-04	1	group
+239	D1C4P9	SciLifeLab_0239	Blue	1	10-04	1	group
+240	D1C4P10	SciLifeLab_0240	Gold	1	10-04	1	group
+241	D1C5P1	SciLifeLab_0241	Red	1	10-05	1	group	Lilac
+242	D1C5P2	SciLifeLab_0242	Orange	1	10-05	1	group
+243	D1C5P3	SciLifeLab_0243	Green	1	10-05	1	group
+244	D1C5P4	SciLifeLab_0244	Navy	1	10-05	1	group
+245	D1C5P5	SciLifeLab_0245	Lilac	1	10-05	1	group
+246	D1C5P6	SciLifeLab_0246	Black	1	10-05	1	group
+247	D1C5P7	SciLifeLab_0247	Silver	1	10-05	1	group
+248	D1C5P8	SciLifeLab_0248	Pink	1	10-05	1	group
+249	D1C5P9	SciLifeLab_0249	Blue	1	10-05	1	group
+250	D1C5P10	SciLifeLab_0250	Gold	1	10-05	1	group
+251	D1C6P1	SciLifeLab_0251	Red	1	10-06	1	group	Blue
+252	D1C6P2	SciLifeLab_0252	Orange	1	10-06	1	group
+253	D1C6P3	SciLifeLab_0253	Green	1	10-06	1	group
+254	D1C6P4	SciLifeLab_0254	Navy	1	10-06	1	group
+255	D1C6P5	SciLifeLab_0255	Lilac	1	10-06	1	group
+256	D1C6P6	SciLifeLab_0256	Black	1	10-06	1	group
+257	D1C6P7	SciLifeLab_0257	Silver	1	10-06	1	group
+258	D1C6P8	SciLifeLab_0258	Pink	1	10-06	1	group
+259	D1C6P9	SciLifeLab_0259	Blue	1	10-06	1	group
+260	D1C6P10	SciLifeLab_0260	Gold	1	10-06	1	group
+261	D1C7P1	SciLifeLab_0261	Red	1	10-07	1	group	Gold
+262	D1C7P2	SciLifeLab_0262	Orange	1	10-07	1	group
+263	D1C7P3	SciLifeLab_0263	Green	1	10-07	1	group
+264	D1C7P4	SciLifeLab_0264	Navy	1	10-07	1	group
+265	D1C7P5	SciLifeLab_0265	Lilac	1	10-07	1	group
+266	D1C7P6	SciLifeLab_0266	Black	1	10-07	1	group
+267	D1C7P7	SciLifeLab_0267	Silver	1	10-07	1	group
+268	D1C7P8	SciLifeLab_0268	Pink	1	10-07	1	group
+269	D1C7P9	SciLifeLab_0269	Blue	1	10-07	1	group
+270	D1C7P10	SciLifeLab_0270	Gold	1	10-07	1	group
+271	D1C8P1	SciLifeLab_0271	Red	1	10-08	1	group	Orange
+272	D1C8P2	SciLifeLab_0272	Orange	1	10-08	1	group
+273	D1C8P3	SciLifeLab_0273	Green	1	10-08	1	group
+274	D1C8P4	SciLifeLab_0274	Navy	1	10-08	1	group
+275	D1C8P5	SciLifeLab_0275	Lilac	1	10-08	1	group
+276	D1C8P6	SciLifeLab_0276	Black	1	10-08	1	group
+277	D1C8P7	SciLifeLab_0277	Silver	1	10-08	1	group
+278	D1C8P8	SciLifeLab_0278	Pink	1	10-08	1	group
+279	D1C8P9	SciLifeLab_0279	Blue	1	10-08	1	group
+280	D1C8P10	SciLifeLab_0280	Gold	1	10-08	1	group
+281	D1C9P1	SciLifeLab_0281	Red	1	10-09	1	group	Navy
+282	D1C9P2	SciLifeLab_0282	Orange	1	10-09	1	group
+283	D1C9P3	SciLifeLab_0283	Green	1	10-09	1	group
+284	D1C9P4	SciLifeLab_0284	Navy	1	10-09	1	group
+285	D1C9P5	SciLifeLab_0285	Lilac	1	10-09	1	group
+286	D1C9P6	SciLifeLab_0286	Black	1	10-09	1	group
+287	D1C9P7	SciLifeLab_0287	Silver	1	10-09	1	group
+288	D1C9P8	SciLifeLab_0288	Pink	1	10-09	1	group
+289	D1C9P9	SciLifeLab_0289	Blue	1	10-09	1	group
+290	D1C9P10	SciLifeLab_0290	Gold	1	10-09	1	group
+291	D1C10P1	SciLifeLab_0291	Red	1	10-10	1	group	Silver
+292	D1C10P2	SciLifeLab_0292	Orange	1	10-10	1	group
+293	D1C10P3	SciLifeLab_0293	Green	1	10-10	1	group
+294	D1C10P4	SciLifeLab_0294	Navy	1	10-10	1	group
+295	D1C10P5	SciLifeLab_0295	Lilac	1	10-10	1	group
+296	D1C10P6	SciLifeLab_0296	Black	1	10-10	1	group
+297	D1C10P7	SciLifeLab_0297	Silver	1	10-10	1	group
+298	D1C10P8	SciLifeLab_0298	Pink	1	10-10	1	group
+299	D1C10P9	SciLifeLab_0299	Blue	1	10-10	1	group
+300	D1C10P10	SciLifeLab_0300	Gold	1	10-10	1	group
+301	D2C1P1	SciLifeLab_0301	Red	2	10-11	1	group	Green
+302	D2C1P2	SciLifeLab_0302	Orange	2	10-11	1	group
+303	D2C1P3	SciLifeLab_0303	Green	2	10-11	1	group
+304	D2C1P4	SciLifeLab_0304	Navy	2	10-11	1	group
+305	D2C1P5	SciLifeLab_0305	Lilac	2	10-11	1	group
+306	D2C1P6	SciLifeLab_0306	Black	2	10-11	1	group
+307	D2C1P7	SciLifeLab_0307	Silver	2	10-11	1	group
+308	D2C1P8	SciLifeLab_0308	Pink	2	10-11	1	group
+309	D2C1P9	SciLifeLab_0309	Blue	2	10-11	1	group
+310	D2C1P10	SciLifeLab_0310	Gold	2	10-11	1	group
+311	D2C2P1	SciLifeLab_0311	Red	2	10-12	1	group	Gold
+312	D2C2P2	SciLifeLab_0312	Orange	2	10-12	1	group
+313	D2C2P3	SciLifeLab_0313	Green	2	10-12	1	group
+314	D2C2P4	SciLifeLab_0314	Navy	2	10-12	1	group
+315	D2C2P5	SciLifeLab_0315	Lilac	2	10-12	1	group
+316	D2C2P6	SciLifeLab_0316	Black	2	10-12	1	group
+317	D2C2P7	SciLifeLab_0317	Silver	2	10-12	1	group
+318	D2C2P8	SciLifeLab_0318	Pink	2	10-12	1	group
+319	D2C2P9	SciLifeLab_0319	Blue	2	10-12	1	group
+320	D2C2P10	SciLifeLab_0320	Gold	2	10-12	1	group
+"""
+
+COLOR_MAP = {
+    'Pink': 'HotPink',
+    'Lilac': 'Plum',
+    'Blue': 'LightSteelBlue',
+    'Navy': 'MediumBlue',
+    'Gold': 'Moccasin',
+    'Silver': 'Gainsboro'
+}
+
+def _color(c):
+    return COLOR_MAP.get(c, c)
+
+def insert_new_pucks():
+    with open_client() as dc:
+        f = StringIO(NEW_PUCKS)
+        last_dewar = 0
+
+        for line in f:
+            if line.strip():
+                parts = line.split()
+                d = int(parts[4])
+                c = int(parts[5].replace('10-', ''))
+                p = int(parts[1].split('P')[1])
+
+                extra = {}
+
+                if d != last_dewar:
+                    extra['dewar'] = {
+                    }
+                    last_dewar = d
+
+                if p == 1:
+                    extra['cane'] = {
+                        'label': parts[5],
+                        'color': _color(parts[8])
+                    }
+
+                puck = {'id': int(parts[0]),
+                        'code': None,
+                        'label': parts[2],
+                        'color': _color(parts[3]),
+                        'dewar': d,
+                        'cane': c,
+                        'position': p,
+                        'extra': extra}
+
+                print("Creating puck: ", puck)
+                dc.request('create_puck', jsonData={'attrs': puck})
+                print(dc.json())
+
+
 def main():
 
     instance = os.environ['EMHUB_INSTANCE']
@@ -163,9 +338,11 @@ def main():
     if update:
         locDict = update_pucks_label()
         update_storage_entries(locDict)
+        insert_new_pucks()
     else:
-        #list_pucks()
-        list_entries()
+        list_pucks()
+        #list_entries()
+        #insert_new_pucks()
 
 
 
