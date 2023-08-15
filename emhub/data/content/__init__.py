88b7f53c8c731bdc6d143d1f0794a5a53486c157
Jose M. de la Rosa <delarosatrevin@gmail.com>
2023-08-15 11:33:32 -0500

    Fixed import

--- emhub/data/content/__init__.py
+++ emhub/data/content/__init__.py
@@ -14,12 +14,13 @@
 # *
 # **************************************************************************
 
+from .dc_base import DataContent, register_content
 from . import (dc_base, dc_raw, dc_users, dc_reports, dc_bookings, dc_invoices,
                dc_projects, dc_sessions)
 
-dc = dc_base.DataContent()
+dc = DataContent()
 
-dc_base.register_content(dc)
+register_content(dc)
 dc_users.register_content(dc)
 dc_bookings.register_content(dc)
 dc_sessions.register_content(dc)
