e5771b5ee51a995f93a61e7b0b1b0409bb43e42f
Jose M. de la Rosa <delarosatrevin@gmail.com>
2024-03-21 16:03:59 -0500

    Fixed forms Redis cache

--- emhub/data/data_models.py
+++ emhub/data/data_models.py
@@ -42,7 +42,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def create_data_models(dm):
-    """ Define the Data Models that will be use by the DataManager. """
+    """ Define the Data Models that will be used by the DataManager. """
 
     Base = dm.Base
 
@@ -878,7 +878,6 @@ def create_data_models(dm):
                     primary_key=True)
 
         name = Column(String(256),
-                      unique=True,
                       nullable=False)
 
         start = Column(UtcDateTime)
