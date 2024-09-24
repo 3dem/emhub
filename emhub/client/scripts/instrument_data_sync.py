d389029cfcdf665771d3a3242612e4e9214287d1
Jose M. de la Rosa <delarosatrevin@gmail.com>
2024-09-24 11:15:09 -0500

    Renamed negstain script and updated it

--- emhub/client/scripts/instrument_data_sync.py
+++ emhub/client/scripts/instrument_data_sync.py
@@ -15,45 +15,60 @@
 # *
 # **************************************************************************
 
-""" 
+"""
+Synchronize
 Monitor a folder with Negative-Stain images and copy them to each group's folder.
 Raw image path and groups folder is read from EMhub configuration.
 """
 
-import os, sys
+import os
+import sys
+import argparse
 from pprint import pprint
 
 from emtools.utils import Color, Process
 from emhub.client import open_client
-from emhub.client.worker import TaskHandler, DefaultTaskHandler, Worker
-
-
-logger = Process.Logger()
-
-def _color(path):
-    color = Color.bold if os.path.exists(path) else Color.red
-    return color(path)
-
-
-def _rsync(src, dst):
-    """ Synchronize content from 'src' to 'dst'. """
-    if not os.path.exists(dst):
-        logger.mkdir(dst)
-    cmd = f"rsync -av '{src}/' '{dst}/'"
-    logger.system(cmd)
-    #print(cmd)
 
 
 def main():
+    p = argparse.ArgumentParser(prog='instrument_data_sync.py')
+    p.add_argument('data_root',
+                   help="Root directory where data is mounted. "
+                        "It is expected that each group has a folder "
+                        "there. ")
+    p.add_argument('instrument_folder',
+                   help="Instrument folder name that will be used "
+                        "to copy the data to. ")
+    p.add_argument('--copy', '-c', action="store_true",
+                   help="By default it will do a dry execution. When "
+                        "this option is used, the real copy is done. ")
+
+    args = p.parse_args()
+    data_root = args.data_root
+    instrument_folder = args.instrument_folder
+    dry = not args.copy
+
+    logger = Process.Logger(only_log=dry)
+
+    def _color(path):
+        color = Color.bold if os.path.exists(path) else Color.red
+        return color(path)
+
+    def _rsync(src, dst):
+        """ Synchronize content from 'src' to 'dst'. """
+        if not os.path.exists(dst):
+            logger.mkdir(dst)
+        cmd = f"rsync -av '{src}/' '{dst}/'"
+        logger.system(cmd)
+
     with open_client() as dc:
-        microscope = 'TalosL120C'
         sconfig = dc.get_config('sessions')
         raw = sconfig['raw']
         jude_folder = raw['jude_group_folder']
         for group in sconfig['groups'].values():
             print('\n', Color.warn(group))
-            negstain_path = os.path.join(raw['root_negstain'], group)
-            print("  NegStain: ", _color(negstain_path))
+            negstain_path = os.path.join(data_root, group)
+            print("  Data root: ", _color(negstain_path))
             gscem_path = os.path.join(raw['root'], group)
             print("     GSCEM: ", _color(gscem_path))
             jude_path = jude_folder.format(group=group)
@@ -63,17 +78,14 @@ def main():
                 continue
 
             if os.path.exists(gscem_path):
-                _rsync(negstain_path, os.path.join(gscem_path, microscope))
+                _rsync(negstain_path, os.path.join(gscem_path, instrument_folder))
 
             if os.path.exists(jude_path):
-                _rsync(negstain_path, os.path.join(jude_path, microscope))
-
+                _rsync(negstain_path, os.path.join(jude_path, instrument_folder))
 
 
 if __name__ == '__main__':
     main()
-    #worker = NegStainWorker(debug=True)
-    #worker.run()
 
 
 
